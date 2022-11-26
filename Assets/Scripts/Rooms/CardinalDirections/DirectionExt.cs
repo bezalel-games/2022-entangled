49689ca9ddd31e230f2eade4291fac557140cefa
Erel Debel <0ereldebel0@gmail.com>
2022-11-26 19:58:31 +0200

    Room System: Added basic room system
    The room system currently consists of a single type of rooms, each has four doors, one for each direction.
    Once the player is mid-way to another room the camera switches to the other room, the room is disabled (with a delay) and all the previous neighbors are entered in to a pool of unused rooms and then taken by the new room to be it's rooms (if there will be more then one type of room, these pools will become more relevant as they will not simply empty all the time).
    Each room has a permanent RoomNode (once it or it's neighbor is first visited) to save the properties of this specific room.

---
+++ Assets/Scripts/Rooms/CardinalDirections/DirectionExt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using static Rooms.CardinalDirections.Direction;
+
+namespace Rooms.CardinalDirections
+{
+    public static class DirectionExt
+    {
+        private const int NumOfDirections = 4;
+
+        public static IEnumerable GetDirections() => (Direction[])Enum.GetValues(typeof(Direction));
+
+        public static Direction Inverse(this Direction dir)
+        {
+            return (Direction)(((byte)dir + 2) % NumOfDirections);
+        }
+
+        public static Direction ToDirection(this Vector2Int dir)
+        {
+            if (dir.x * dir.y != 0 || (dir.x == 0 && dir.y == 0))
+                MonoBehaviour.print($"direction not supported: {dir}");
+            if (dir.x != 0) return dir.x > 0 ? East : West;
+            return dir.y > 0 ? North : South;
+        }
+
+        public static Vector2Int ToVector(this Direction dir)
+        {
+            return dir switch
+            {
+                East => Vector2Int.right,
+                West => Vector2Int.left,
+                North => Vector2Int.up,
+                South => Vector2Int.down,
+                _ => throw new ArgumentOutOfRangeException(nameof(dir), dir, null)
+            };
+        }
+    }
+}
--- Assets/Scripts/Rooms/Room.cs
+++ Assets/Scripts/Rooms/Room.cs
@@ -1,24 +1,68 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using Cinemachine;
+using UnityEngine;
 
 namespace Rooms
 {
     public class Room : MonoBehaviour
     {
-        [SerializeField] private RoomNode node;
+        #region Serialized Fields
+
+        [SerializeField] private int inPriority;
+        [SerializeField] private int outPriority;
+        [SerializeField] private CinemachineVirtualCamera vCam;
+        [SerializeField] private GameObject roomContent;
+
+        #endregion
+
+        #region Non-Serialized Fields
+
+        private Collider2D _collider;
+
+        #endregion
+
+        #region Properties
+
+        public RoomNode Node { get; set; }
+
+        #endregion
 
         #region Function Events
-        
-        private void OnBecameVisible() //should be replaced
+
+        private void OnTriggerEnter2D(Collider2D col)
         {
-            RoomManager.CurrentRoom = node;
+            RoomManager.ChangeRoom(Node);
         }
-        
+
+        #endregion
+
+        #region Public Methods
+
+        public void Enter()
+        {
+            vCam.Priority = inPriority;
+            roomContent.SetActive(true);
+        }
+
+        public void Exit(float sleepDelay = 0)
+        {
+            vCam.Priority = outPriority;
+            if (sleepDelay <= 0)
+                roomContent.SetActive(false);
+            else
+                StartCoroutine(SleepWithDelay(sleepDelay));
+        }
+
         #endregion
 
         #region Private Methods
-        
-        
-        
+
+        private IEnumerator SleepWithDelay(float sleepDelay)
+        {
+            yield return new WaitForSeconds(sleepDelay);
+            roomContent.SetActive(false);
+        }
+
         #endregion
     }
 }
