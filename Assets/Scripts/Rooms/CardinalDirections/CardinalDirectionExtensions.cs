b9ca48c37b875ef81f5d0c45a1dda0bf28c196bb
Erel Debel <0ereldebel0@gmail.com>
2022-11-26 09:00:05 +0200

    added first room

---
+++ Assets/Scripts/Rooms/CardinalDirections/CardinalDirectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Rooms.CardinalDirections
+{
+    public static class CardinalDirectionExtensions
+    {
+        private const int NumOfDirections = 4;
+
+        public static CardinalDirection Inverse(this CardinalDirection dir)
+        {
+            return (CardinalDirection)((short)dir + 2 % NumOfDirections);
+        }
+    }
+}
---
+++ Assets/Scripts/Rooms/Room.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Rooms
+{
+    public class Room : MonoBehaviour
+    {
+        [SerializeField] private RoomNode node;
+
+        #region Function Events
+        
+        private void OnBecameVisible() //should be replaced
+        {
+            RoomManager.CurrentRoom = node;
+        }
+        
+        #endregion
+
+        #region Private Methods
+        
+        
+        
+        #endregion
+    }
+}
