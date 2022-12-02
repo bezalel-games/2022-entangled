859262f1c9814c53aa9eff1662f34289786d133e
Erel Debel <0ereldebel0@gmail.com>
2022-12-02 17:26:48 +0200

    Added Enemy placeholder
    Enemies in every room are chosen randomly such that the sum of their ranks would be equal to the rank of their ranks.
    Enemies in every room will restart (but stay the same enemies) when you return to a room you have already entered.
    "Enemies" are disabled on trigger enter.

---
+++ Assets/Scripts/Enemies/EnemyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemies
+{
+    [CreateAssetMenu(fileName = "Enemy Dictionary Asset", menuName = "Enemies Asset", order = 0)]
+    public class EnemyDictionary : ScriptableObject
+    {
+        [field: SerializeField] private Enemy[] Enemies { get; set; }
+
+        public Enemy this[int index]
+        {
+            get => Enemies[index];
+            set => Enemies[index] = value;
+        }
+
+        public int Count => Enemies.Length;
+
+        private void OnValidate()
+        {
+            if (Enemies == null) return;
+            Array.Sort(Enemies, (a, b) => Comparer<int>.Default.Compare(a.Rank, b.Rank));
+        }
+
+        public int GetMaxIndexForRank(int rank)
+        {
+            for (int i = Enemies.Length - 1; i > 0; i--)
+                if (Enemies[i].Rank <= rank)
+                    return i;
+            return 0;
+        }
+
+        public int GetRankByIndex(int index)
+        {
+            return Enemies[index].Rank;
+        }
+    }
+}
