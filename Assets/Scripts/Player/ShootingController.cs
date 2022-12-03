31288ab3976daa65ba6e6fff15ae02ed678aab71
Alon Levy <alon153@gmail.com>
2022-12-03 15:25:36 +0200

    Precition Shot: Created

--- Assets/Scripts/Player/ShootingController.cs
+++ Assets/Scripts/Player/ShootingController.cs
@@ -6,12 +6,14 @@ using UnityEngine.UIElements;
 public partial class PlayerController
 {
     #region Serialized Fields
-    
-    [Header("Shooting")] 
-    [SerializeField] private float _rotationSpeed;
+
+    [Header("Shooting")] [SerializeField] private float _rotationSpeed;
     [SerializeField] private GameObject _aimLine;
     [SerializeField] private GameObject _aimPivot;
-    
+
+    [Header("Precision Shot")] [SerializeField]
+    private float _precisionTime;
+
     #endregion
 
     #region Non-Serialized Fields
@@ -19,6 +21,8 @@ public partial class PlayerController
     private Vector2 _aimDirection;
 
     private Yoyo _yoyo;
+
+    private bool _precisioning;
 
     #endregion
 
@@ -31,7 +35,7 @@ public partial class PlayerController
     #region Function Events
 
     #endregion
-    
+
     #region Input Actions
 
     public void OnAim(InputAction.CallbackContext context)
@@ -57,17 +61,28 @@ public partial class PlayerController
                 {
                     _yoyo.Shoot(_aimDirection);
                 }
+
                 break;
         }
     }
 
     public void OnPresicionShot(InputAction.CallbackContext context)
     {
-        throw new System.NotImplementedException();
+        switch (context.phase)
+        {
+            case InputActionPhase.Started:
+                _yoyo.PrecisionShoot();
+                DelayInvoke(
+                    () => {_yoyo.CancelPrecision();}, _precisionTime);
+                break;
+            case InputActionPhase.Canceled:
+                _yoyo.CancelPrecision();
+                break;
+        }
     }
 
     #endregion
-    
+
     #region Public Methods
 
     #endregion
@@ -76,22 +91,28 @@ public partial class PlayerController
 
     private void SetAim()
     {
-        if (_aimDirection != Vector2.zero)
+        if (_yoyo.State == Yoyo.YoyoState.PERCISION)
         {
-            var zRotation = Vector3.SignedAngle(_aimDirection, Vector3.up, -Vector3.forward);
-            var q = Quaternion.Euler(0,0,zRotation);
-            if (!Aiming)
+            _yoyo.PrecisionDirection = _aimDirection;
+        }
+        else
+        {
+            if (_aimDirection != Vector2.zero)
             {
-                _aimPivot.transform.rotation = q;
-                _aimLine.SetActive(true);
-            }
-            else
-            {
-                _aimPivot.transform.rotation = Quaternion.Slerp(_aimPivot.transform.rotation, q, Time.deltaTime * _rotationSpeed);
+                var zRotation = Vector3.SignedAngle(_aimDirection, Vector3.up, -Vector3.forward);
+                var q = Quaternion.Euler(0, 0, zRotation);
+                if (!Aiming)
+                {
+                    _aimPivot.transform.rotation = q;
+                    _aimLine.SetActive(true);
+                }
+                else
+                {
+                    _aimPivot.transform.rotation =
+                        Quaternion.Slerp(_aimPivot.transform.rotation, q, Time.deltaTime * _rotationSpeed);
+                }
             }
         }
-
-
     }
 
     #endregion
