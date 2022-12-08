46898740014b994658b37af50cdefe82bf9c60b1
Erel Debel <0ereldebel0@gmail.com>
2022-12-08 10:30:58 +0200

    Moved animations to Animations folder. used Aviad's tiles. added animator component to character

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -95,7 +95,7 @@ public class GameManager : MonoBehaviour
 
     public static void RoomCleared()
     {
-        _instance.ChooseCard();
+        // _instance.ChooseCard();
     }
 
     public static void CardChosen()
