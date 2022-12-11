76ea33e4b94043b61fa294238d33523e1afaf8c7
Erel Debel <0ereldebel0@gmail.com>
2022-12-11 13:45:11 +0200

    Created Card monobehaviour and buff and debuff interfaces

--- Assets/Scripts/Cards/Buffs/ActiveBuffs/BlowableYoyo.cs
+++ Assets/Scripts/Cards/Buffs/ActiveBuffs/BlowableYoyo.cs
@@ -1,19 +1,24 @@
-﻿using Player;
+﻿using System;
+using Player;
+using Player.Yoyo;
 using UnityEngine.InputSystem;
 
 namespace Cards.Buffs.ActiveBuffs
 {
-    public class BlowableYoyo : Buff 
+    public class BlowableYoyo : IBuff 
     {
-        protected override void ApplyBuff(PlayerController playerController)
+        private Yoyo _yoyo;
+
+        public void Apply(PlayerController playerController)
         {
+            _yoyo = playerController.Yoyo;
             playerController.QuickShotEvent += BlowUpYoyo;
         }
 
         private void BlowUpYoyo(InputActionPhase phase)
         {
             if (phase is not InputActionPhase.Started) return;
-            
+            throw new NotImplementedException("what is the hit interface?");
         }
     }
 }
---
+++ Assets/Scripts/Cards/Card.cs
@@ -0,0 +1,48 @@
+﻿using Cards.Buffs;
+using Cards.Debuffs;
+using Managers;
+using Rooms;
+using UnityEngine;
+
+namespace Cards
+{
+    public class Card : MonoBehaviour
+    {
+        #region Serialized Fields
+
+        [SerializeField] private IBuff _buff;
+        [SerializeField] private IDebuff _debuff;
+
+        #endregion
+
+        #region Non-Serialized Fields
+
+        private bool _applied = false;
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Function Events
+
+        #endregion
+
+        #region Public Methods
+
+        public void Apply()
+        {
+            if (_applied) return;
+            _buff.Apply(GameManager.PlayerController);
+            _debuff.Apply(RoomManager.EnemyDictionary);
+            _applied = true;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+    }
+}
