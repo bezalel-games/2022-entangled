89fd767c4fe063e713791cfd4f686439858adfbd
Alon Levy <alon153@gmail.com>
2022-12-18 21:42:34 +0200

    Enemies: Added static attacking counter

--- Assets/Scripts/Enemies/Shooter/ShooterPrepareAttack.cs
+++ Assets/Scripts/Enemies/Shooter/ShooterPrepareAttack.cs
@@ -7,6 +7,7 @@ namespace Enemies.Shooter
         public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             base.OnStateEnter(animator, stateInfo, layerIndex);
+            Enemy.NumberOfAttacking++;
             SetSpeedMultiplier(animator, stateInfo, "Prepare Speed", ThisEnemy.PrepareAttackTime);
         }
     }
