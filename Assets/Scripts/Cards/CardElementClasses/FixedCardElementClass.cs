26bd6f5a6f0cceef39e799521938ae0feed4b5c7
Erel Debel <0ereldebel0@gmail.com>
2022-12-20 21:05:54 +0200

    Changed to always use float values in buff/debuff parameters for type simplicity. CardElementClass is now a regular serialized class. only ClassElementCardAttributes is a scriptable object.

---
+++ Assets/Scripts/Cards/CardElementClasses/FixedCardElementClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+using static Cards.Rarity;
+
+namespace Cards.CardElementClasses
+{
+    [Serializable]
+    public class FixedCardElementClass
+    {
+        [SerializeField] private Rarity _rarity;
+        [SerializeField] private float _parameter;
+        [field: SerializeField] public CardElementClassAttributes Attributes { get; private set; }
+
+        public void ToInt()
+        {
+            _parameter = (int)_parameter;
+        }
+    }
+}
