93f591721855ac98d1d58a660ec729590bb6e74b
Erel Debel <0ereldebel0@gmail.com>
2022-12-21 13:20:51 +0200

    Changed Card structure to compose of 2 parts and a rarity identifier. As a result, changed button transitions to be animations that change the card's size

---
+++ Assets/Scripts/UI/CardUI.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Cards;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class CardUI : MonoBehaviour
+    {
+        #region Serialized Fields
+        
+        [SerializeField] private string _titleFormat;
+        [SerializeField] private string _buffFormat;
+        [SerializeField] private string _debuffFormat;
+        [SerializeField] private RarityColors _rarityColors;
+
+        #endregion
+        #region Non-Serialized Fields
+
+        private Image _buffImage;
+        private Image _debuffImage;
+        private TextMeshProUGUI _buffText;
+        private TextMeshProUGUI _debuffText;
+        private Image _rarityIdentifier;
+
+        #endregion
+
+        #region Properties
+
+        public Card Card
+        {
+            set
+            {
+                // value.TitleString(_titleFormat);
+                _buffText.text = value.BuffString(_buffFormat);
+                _debuffText.text = value.DebuffString(_debuffFormat);
+                _rarityIdentifier.color = _rarityColors[value.Rarity];
+            }
+        }
+
+        #endregion
+
+        #region Function Events
+
+        private void Awake()
+        {
+            var buffObject = transform.GetChild(0);
+            _buffImage = buffObject.GetComponent<Image>();
+            _buffText = buffObject.GetComponentInChildren(typeof(TextMeshProUGUI), true) as TextMeshProUGUI;
+            var debuffObject = transform.GetChild(1);
+            _debuffImage = debuffObject.GetComponent<Image>();
+            _debuffText = debuffObject.GetComponentInChildren(typeof(TextMeshProUGUI), true) as TextMeshProUGUI;
+            _rarityIdentifier = transform.GetChild(2).GetComponent<Image>();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+        
+        #region Class
+
+        [Serializable]
+        public class RarityColors
+        {
+            [SerializeField] private Color _commonColor;
+            [SerializeField] private Color _rareColor;
+            [SerializeField] private Color _epicColor;
+
+            public Color this[Rarity rarity] => rarity switch
+            {
+                Rarity.COMMON=>_commonColor,
+                Rarity.RARE=>_rareColor,
+                Rarity.EPIC=>_epicColor,
+                _ => throw new ArgumentOutOfRangeException(nameof(rarity), rarity, null)
+            };
+        }
+        
+        #endregion
+    }
+}
