d51103f4bd92f8b0657a6d43682de404ae8d5ba9
Erel Debel <0ereldebel0@gmail.com>
2023-01-01 23:44:56 +0200

    Started work on fumer enemy

---
+++ Assets/Scripts/Enemies/Fumer/FumerMove.cs
@@ -0,0 +1,10 @@
+namespace Enemies
+{
+    public class FumerMove : MoveBehaviour<Fumer>
+    {
+        // protected override Vector2 GetToPlayerDirection()
+        // {
+        //     return (Player.position - ThisEnemy.transform.position).normalized;
+        // }
+    }
+}
