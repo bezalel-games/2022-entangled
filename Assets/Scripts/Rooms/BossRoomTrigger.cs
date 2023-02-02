11659714ed1208ff91a678815394f0d8999a769d
Erel Debel <0ereldebel0@gmail.com>
2023-02-02 12:47:07 +0200

    Added boss room trigger for minimap

---
+++ Assets/Scripts/Rooms/BossRoomTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Rooms
+{
+    public class BossRoomTrigger : MonoBehaviour
+    {
+        private const string MINIMAP_PATH = "/UI/Minimap";
+
+        private void OnEnable()
+        {
+            GameObject.Find(MINIMAP_PATH).SetActive(false);
+        }
+    }
+}
